b454e4beda12930226e33eb3f11be2b613e46b30
CorneL <lazacmd@yahoo.com>
2017-10-06 19:23:32 +0300

    add RolesRepository

---
+++ app/Repositories/RolesRepository.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Corneliu
+ * Date: 06.10.2017
+ * Time: 19:12
+ */
+
+namespace App\Repositories;
+
+use App\Role;
+
+class RolesRepository extends SiteRepository
+{
+    public function  __construct(Role $role)
+    {
+        $this->model = $role;
+    }
+}
